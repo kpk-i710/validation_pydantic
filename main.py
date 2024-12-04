b746df4b0cd9c6aab4ec42fdc4f25f494c5a350e
kpk-i710 <47340012+kpk-i710@users.noreply.github.com>
2024-12-04 06:19:39 +0600

    добавиил baseмодель iserSchema

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+from  pydantic import  BaseModel
+
 data = {
     "email": "abs@email",
     'bio': None,
     "age": 12
 }
+
+
+class UserSchema(BaseModel):
+    email:str
+    bio:str
+    age:str
+
+
+def func(data_: dict):
+    data_["age"] += 1

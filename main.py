b16f3b1c307876f4ac686fe1728d8cd5a56d0873
kpk-i710 <47340012+kpk-i710@users.noreply.github.com>
2024-12-04 06:24:32 +0600

    добавил валидацию данных с 0 до 130 циыру

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from  pydantic import  BaseModel
+from  pydantic import  BaseModel, Field
 
 data = {
     "email": "abs@email",
@@ -9,9 +9,11 @@ data = {
 
 class UserSchema(BaseModel):
     email:str
-    bio:str
-    age:str
+    bio:str | None
+    age: int = Field(ge=0,le=130)
 
 
 def func(data_: dict):
     data_["age"] += 1
+
+print(UserSchema(**data))
